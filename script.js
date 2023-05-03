6ffe26f2b415e300e80a891d88d07e1792d17b2b
ninotal90 <ntalakvadze90@gmail.com>
2023-05-03 13:45:14 +0400

    Added html and javascript

---
+++ script.js
@@ -0,0 +1,102 @@
+// 1.
+let x = 5;
+while(true) {
+   console.log(x);
+   x++
+   if (x == 101) {
+     break
+   }
+}
+
+// 2.
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let x = 0; x < array1.length; x++) {
+//   console.log(x);
+//   if (array1[x] > 0 && (array1[x]) < 10 ) {
+//     console.log(array1[x]);
+//   }
+// }
+
+// 3.
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//  for (let x of array2) {
+//    if (x == 5) {
+//       break;
+//    }
+//    console.log(x);
+//  }
+
+// 4. ----------
+// 5. ----------
+
+// 6.
+// let array5 = [1, 2, 3, 7, 6, 9];
+// for (item of array5) {
+//   if (item == 7) {
+//      continue;
+//   }
+//   console.log(item);
+// }
+
+// 7.
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+//     console.log(user.studentstatus);
+
+// 8.
+//  let user = {
+//     name: "giorgi",
+//     age: 20,
+//     studentstatus: "active",
+//   };
+
+//   if (user["age"] < 18 && (user["studentstatus"] = "active")) {
+//     console.log("hello");
+//   } else if (user["name"] == "levan") {
+//     console.log("hello levan");
+//   } else if ((user["studentstatus"] = "active" || user["age"] < 25)) {
+//     console.log("hello world");
+//   } else {
+//     console.log("error");
+//   }
+
+//  9.
+// let array = [
+//     [2, -3, 5, 10],
+//     [25, -24, -11, 100],
+//     [-6, -7, 10],
+//   ];
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     // console.log(element);
+//     for (let n of element) {
+//       if (n > 0) {
+//         console.log(n);
+//       }
+//     }
+//   }
+
+//  10. ლუწი
+// let array = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
+// for (let x of array) {
+//   if (array[x] % 2 == 0) {
+//     console.log(array[x]);
+//   }
+// } 
+
+// კენტი
+// let array = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
+// for (let item of array) {
+//   if (array[item] % 2==1) {
+//     console.log(array[item]);
+//   }
+// }
+
+
+
+//  11. -------
